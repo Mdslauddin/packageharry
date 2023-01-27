89c4de725c4fcb488f68a55391c1e35348ecf5a2
Mdslauddin <mdslauddin285@.com>
2023-01-27 09:35:05 +0530

    first commit

---
+++ packageharry/__init__.py
@@ -0,0 +1,6 @@
+def func(number):
+   
+    print("This is function ")
+  
+    return number
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='package name',
+version='0.1.1',
+description="This code with harry",
+long_description='This is very very long description',
+author='Harry',
+packages=['packageharry'],
+instaall_require=[])
